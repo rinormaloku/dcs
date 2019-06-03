7e8ac8a2bcad0c3335c91936e7763e71e17c6c02
rinormaloku <rinormaloku37@gmail.com>
2019-06-03 15:16:59 +0200

    push the button

---
+++ setup.sh
@@ -0,0 +1,24 @@
+IMAGE_NAME=$1
+
+cat << EOF > ./docker-compose.yml
+version: "3"
+
+services:
+  suprb:
+    image: $IMAGE_NAME
+    ports:
+      - "8090:80"
+    # Update with real application
+
+  reverse-proxy:
+    image: traefik:1.7-alpine
+    ports:
+      - "80:80"
+      - "443:443"
+    command: --api
+      - "80:80" 
+      - "443:443" 
+    volumes:
+      - ./traefik:/etc/traefik       # Traefik configuration 
+      - ./volumes/traefik-acme:/acme # SSL Certification store
+EOF
